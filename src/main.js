c75278ea76a279f792e28bb2ec378489be9c0e49
sunzilian <757338031@qq.com>
2020-03-30 15:39:51 +0800

    add element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import api from '@/api';
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
+import './styles.scss'
+
+Vue.use(ElementUI, { locale })
 
 Vue.prototype.$api = api;
 
