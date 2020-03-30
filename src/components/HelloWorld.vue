2431c65073f325c96dcb463f7ba68be29bbcda7e
sunzilian <757338031@qq.com>
2020-03-30 15:05:23 +0800

    add stylus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,9 +40,11 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="stylus" scoped>
+$color=red
 h3 {
   margin: 40px 0 0;
+  color $color
 }
 ul {
   list-style-type: none;
@@ -55,4 +57,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
